e931a6483845e735ebdd808e62f724449477de24
lsegaux <laetitia_segaux@hotmail.com>
2020-10-07 17:46:54 -0400

    Add button to select file

--- src/main/java/controller/LoginInfoController.java
+++ src/main/java/controller/LoginInfoController.java
@@ -1,5 +1,9 @@
 package controller;
 
+import java.awt.*;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 
 import javafx.event.ActionEvent;
@@ -9,14 +13,20 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
+import javafx.stage.Popup;
+
+import javax.swing.filechooser.FileNameExtensionFilter;
+
 
 public class LoginInfoController {
 	
 	@FXML private Label user;
 	@FXML private Label pass;
 	@FXML private Label date;
-	
+	private Desktop desktop = Desktop.getDesktop();
+
 	public void setUser(String s) {
 		user.setText(s);
 	}
@@ -28,7 +38,7 @@ public class LoginInfoController {
 	public void setDate(String s) {
 		date.setText(s);
 	}
-	
+
 	//going to forgot password scene
 	public void goToLoginPage(ActionEvent event) throws IOException {
 		Parent forgotPassword = FXMLLoader.load(getClass().getResource("/view/login.fxml"));
@@ -39,4 +49,28 @@ public class LoginInfoController {
 		window.setScene(forgotPasswordScene);
 		window.show();
 	}
+
+	public void addHouseLayout(ActionEvent event) throws IOException {
+
+		Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+		FileChooser fileChooser = new FileChooser();
+		FileChooser.ExtensionFilter extFilter =
+				new FileChooser.ExtensionFilter("TEXT files (*.txt)", "*.txt");
+		fileChooser.getExtensionFilters().add(extFilter);
+		fileChooser.setTitle("Open Resource File");
+		File file = fileChooser.showOpenDialog(window);
+
+		if (file != null) System.out.println(":)");
+
+
+	}
+
+//	private void openFile(File file) {
+//		try {
+//			desktop.open(file);
+//		}
+//		catch (Exception e){
+//			System.out.println("uh oh");
+//		}
+//	}
 }
