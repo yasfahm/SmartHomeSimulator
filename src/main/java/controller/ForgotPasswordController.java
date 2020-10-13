0933ecee740c7b4318a2148b118991f459e3a78f
Pasha Pishdad <pasha.pishdad@gmail.com>
2020-10-12 23:34:12 -0400

    issue 10,11,12,13 creating a close button on all pages and the ability to move the window in each page. created a label to show the proper message in login, sign up and forgot password page. added comments for login, loginInfo, signUp, forgotPassword page.

--- src/main/java/controller/ForgotPasswordController.java
+++ src/main/java/controller/ForgotPasswordController.java
@@ -2,34 +2,110 @@ package controller;
 
 import java.io.IOException;
 
+import constants.RegistrationStatus;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import service.RegistrationService;
 
 public class ForgotPasswordController {
+	/**
+	 * declaring variables
+	 */
 	@FXML
 	private TextField username;
 	@FXML
 	private PasswordField password;
 	@FXML
 	private PasswordField passwordVerification;
+	@FXML
+	private Label displayMessage;
 
+	private double xOffset = 0;
+	private double yOffset = 0;
+
+	/**
+	 * This function loads the login page(scene) into the window(stage)
+	 * @param event
+	 * @throws IOException
+	 */
 	public void goToLoginPage(ActionEvent event) throws IOException {
 		Parent forgotPassword = FXMLLoader.load(getClass().getResource("/view/login.fxml"));
 		Scene forgotPasswordScene = new Scene(forgotPassword);
 
-		RegistrationService.updatePassword(username.getText(), password.getText(), passwordVerification.getText());
-
-		// stage info
+		/**
+		 * Stage info
+		 */
 		Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
 		window.setScene(forgotPasswordScene);
 		window.show();
+	}
+
+	/**
+	 * This function update the user's password and shows the proper message.
+	 * @param event
+	 */
+	public void updatePassword(ActionEvent event) {
+		RegistrationStatus status = RegistrationService.updatePassword(username.getText(), password.getText(), passwordVerification.getText());
+		switch (status) {
+			case PASSWORD_UPDATED: {
+				displayMessage.setTextFill(Color.BLACK);
+				displayMessage.setText("Password has been updated");
+				//setPrompt(Alert.AlertType.INFORMATION, "Password has been created");
+				break;
+			}
+			case PASSWORD_NOT_EQUAL: {
+				displayMessage.setTextFill(Color.RED);
+				displayMessage.setText("Passwords are not equal");
+				break;
+			}
+			case USERNAME_NOT_FOUND: {
+				displayMessage.setTextFill(Color.RED);
+				displayMessage.setText("Username not found");
+				break;
+			}
+			default: {
+				displayMessage.setTextFill(Color.RED);
+				displayMessage.setText("Seems like an error occurred");
+			}
+		}
+	}
+
+	/**
+	 * This function will close the application
+	 * @param event
+	 * @throws IOException
+	 */
+	public void close(MouseEvent event) throws IOException {
+		System.exit(0);
+	}
+
+	/**
+	 * Gets the location of a mouse.
+	 * @param event
+	 */
+	public void handle(MouseEvent event) {
+		xOffset = event.getSceneX();
+		yOffset = event.getSceneY();
+	}
+
+	/**
+	 * Changes the location of the window(stage) based on the mouse location..
+	 * @param event
+	 */
+	public void move(MouseEvent event) {
+		Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+		window.setX(event.getScreenX() - xOffset);
+		window.setY(event.getScreenY() - yOffset);
 	}
 }
--- src/main/java/controller/LoginController.java
+++ src/main/java/controller/LoginController.java
@@ -6,6 +6,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
@@ -19,12 +20,21 @@ import java.util.Map;
 import java.util.Objects;
 
 public class LoginController {
-	
+	/**
+	 * declaring variables
+	 */
 	@FXML private TextField userD;
 	@FXML private PasswordField passD;
-	
-	
-	//going to forgot password scene
+	@FXML private Label displayMessage;
+
+	private double xOffset = 0;
+	private double yOffset = 0;
+
+	/**
+	 * This function loads the forgot password page(scene) into the window(stage)
+	 * @param event
+	 * @throws IOException
+	 */
 	public void goToForgotPassword(ActionEvent event) throws IOException {
 		Parent forgotPassword = FXMLLoader.load(getClass().getResource("/view/forgotPassword.fxml"));
 		Scene forgotPasswordScene = new Scene(forgotPassword);
@@ -34,8 +44,12 @@ public class LoginController {
 		window.setScene(forgotPasswordScene);
 		window.show();
 	}
-	
-	//going to sign up scene
+
+	/**
+	 * This function loads the sign up page(scene) into the window(stage)
+	 * @param event
+	 * @throws IOException
+	 */
 	public void goToSignUp(ActionEvent event) throws IOException {
 		Parent signUp = FXMLLoader.load(getClass().getResource("/view/signUp.fxml"));
 		Scene signUpScene = new Scene(signUp);
@@ -44,8 +58,12 @@ public class LoginController {
 		Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
 		window.setScene(signUpScene);window.show();
 	}
-	
-	//show the login info
+
+	/**
+	 * This function loads the loginInfo page(scene) into the window(stage)
+	 * @param event
+	 * @throws IOException
+	 */
 	public void loginInfo(ActionEvent event) throws IOException {
 		FXMLLoader loader = new FXMLLoader();
 		loader.setLocation(getClass().getResource("/view/loginInfo.fxml"));
@@ -73,12 +91,36 @@ public class LoginController {
 		}
 		else {
 			// Create Popup with no user correlating to given username and password
-			System.out.println("Incorrect username and password");
+			displayMessage.setText("Incorrect username and password");
 		}
 
 	}
-		
+
+	/**
+	 * This function will close the application
+	 * @param event
+	 * @throws IOException
+	 */
 	public void close(MouseEvent event) throws IOException {
 		System.exit(0);
+	}
+
+	/**
+	 * Gets the location of a mouse.
+	 * @param event
+	 */
+	public void handle(MouseEvent event) {
+		xOffset = event.getSceneX();
+		yOffset = event.getSceneY();
+	}
+
+	/**
+	 * Changes the location of the window(stage) based on the mouse location..
+	 * @param event
+	 */
+	public void move(MouseEvent event) {
+		Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+		window.setX(event.getScreenX() - xOffset);
+		window.setY(event.getScreenY() - yOffset);
 	}
 }
